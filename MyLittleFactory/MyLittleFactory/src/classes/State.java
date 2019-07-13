029e40c1a782a41ef56e16c5ae9f8532bf887920
Christoph Hanauer <Christoph.Hanauer@uni-kassel.de>
2019-07-13 13:31:38 +0200

    update

--- MyLittleFactory/MyLittleFactory/src/classes/State.java
+++ MyLittleFactory/MyLittleFactory/src/classes/State.java
@@ -5,11 +5,20 @@ import java.util.ArrayList;
 public class State {
 
 	String desc;
-	ArrayList <State> neighbors; 
+	ArrayList <State> neighbors;
+	boolean persistent = false;
 	
 	public State(String description) {
 		desc = description;
 		neighbors = new ArrayList<State>();
+	}
+	
+	public void setPersistent(boolean p) {
+		persistent = p;
+	}
+	
+	public boolean isPersistent() {
+		return persistent;
 	}
 	
 	public void addNeighbors(State s) {
